64b132d158dee7aa71a526e46b9cbd6a079ed57d
MariusGi <mariusgirnis@gmail.com>
2020-08-11 00:19:23 +0300

    add autoload

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use TaskApp\TestClass;
+
+$animal = new TestClass();
+echo $animal->roar();
---
+++ src/TestClass.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace TaskApp;
+
+class TestClass
+{
+    public function roar()
+    {
+        return 'Roaring now...';
+    }
+}
